6456b976edaca169555a93566c8d46bd7f78209a
scotthartbti <scott.hart.bti@gmail.com>
2013-03-02 13:55:20 -0800

    Revert "Frameworks: Moveable navbar (1/2)"
    This reverts commit 761b4fb88ad3faa917c58217b44c23a21b5a2b43.
    
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: Iaa3c32aac96a4fa32d18b34a572420caf4093f8f

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -668,8 +668,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 	    resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_SHOW), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
-	    	    Settings.System.NAVIGATION_BAR_CAN_MOVE), false, this);
-            resolver.registerContentObserver(Settings.System.getUriFor(
         Settings.System.NAVIGATION_BAR_HEIGHT), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
         Settings.System.NAVIGATION_BAR_HEIGHT_LANDSCAPE), false, this);
@@ -1320,12 +1318,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (shortSizeDp < 600) {
             // 0-599dp: "phone" UI with a separate status & navigation bar
             mHasSystemNavBar = false;
-            if (Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.NAVIGATION_BAR_CAN_MOVE, 1) == 1) {
-                mNavigationBarCanMove = true;
-            } else {
-                mNavigationBarCanMove = false;
-            }
+            mNavigationBarCanMove = true;
 	    Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.TABLET_UI, 0);
         } else if (shortSizeDp < 720) {
@@ -1513,9 +1506,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.VOLBTN_MUSIC_CONTROLS, 1, UserHandle.USER_CURRENT) == 1); 
 	    mBackKillTimeout = Settings.System.getInt(resolver,
                     Settings.System.KILL_APP_LONGPRESS_TIMEOUT, 1500);
-
-                mNavigationBarCanMove = (Settings.System.getInt(resolver,
-                        Settings.System.NAVIGATION_BAR_CAN_MOVE, 1) == 1);
 
            boolean keyRebindingEnabled = Settings.System.getInt(resolver,
                     Settings.System.HARDWARE_KEY_REBINDING, 0) == 1;
