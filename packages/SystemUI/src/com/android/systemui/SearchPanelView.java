84a49320d17572bd86ecfb3e06efaee7f1c4f3a9
scotthartbti <scott.hart.bti@gmail.com>
2013-03-02 13:51:48 -0800

    Revert "Fix memory leak"
    This reverts commit e83f1a30400990cb72e55ecaabf69f4bbd8a0855.

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -120,7 +120,6 @@ public class SearchPanelView extends FrameLayout implements
     private PackageManager mPackageManager;
     private Resources mResources;
     private TargetObserver mTargetObserver;
-    private SettingsObserver mSettingsObserver;
     private ContentResolver mContentResolver;
     private String[] targetActivities = new String[5];
     private String[] longActivities = new String[5];
@@ -151,18 +150,10 @@ public class SearchPanelView extends FrameLayout implements
         mContentResolver = mContext.getContentResolver();
 
         mcyanogenmodTarget = new cyanogenmodTarget(context);
-        mSettingsObserver = new SettingsObserver(new Handler());
-    }
 
-    @Override
-    protected void onAttachedToWindow() {
-        mSettingsObserver.observe();
+        SettingsObserver observer = new SettingsObserver(new Handler());
+        observer.observe();
         updateSettings();
-    }
-
-    @Override
-    protected void onDetachedFromWindow() {
-        mContentResolver.unregisterContentObserver(mSettingsObserver);
     }
 
     private void startAssistActivity() {
